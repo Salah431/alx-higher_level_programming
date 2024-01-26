728673c823c3160d640a9e1bc11754ad5081892b
Salah <salahayman431@gmail.com>
2024-01-26 03:33:16 +0200

    py

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a GET request to the URL, and display only body of a 200 status code response
-curl -s "$1" -X GET -L
+# Bash script that takes in a URL, sends a GET request to the URL
+curl -sL "$1"
